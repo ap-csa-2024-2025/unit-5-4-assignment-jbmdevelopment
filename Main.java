5877e4b63856aecf5d643a2af888e1514c440a21
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-27 10:41:05 -0700

    aaaa

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-    // test your functions here to make sure they work
+public class Main {
+  public static double distance(int x, int y, int x2, int y2) {
+     final int xDist = x2 - x;
+     final int yDist = y2 - y;
+     final int xSQ = xDist * xDist;
+     final int ySQ = yDist * yDist;
+     final int total = xSQ + ySQ;
+     final double dist = Math.sqrt(total);
+     return dist;
   }
+  public static void main(String[] args) {
+    System.out.print(distance(0, 5, 0, 0)); // expected 5.0
+  }
+    // test your functions here to make sure they work
 
   // Implement distance formula method here
 
