c4bcd5d243c71660e63cecbcd62fd0b0cfb149aa
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-27 10:48:00 -0700

    aaa

--- Main.java
+++ Main.java
@@ -8,8 +8,16 @@ public class Main {
      final double dist = Math.sqrt(total);
      return dist;
   }
+  public static double quadratic(double a, double b, double c) {
+    final double opposite = b * -1;
+    final double discr = Marh.sqrt(b * b - 4 * a * c);
+    final double equation1 =  ((double) opposite + discr) / (2 * a);
+    final double equation2 =  ((double) opposite + discr) / (2 * a);
+    return equation1;
+  }
   public static void main(String[] args) {
     System.out.print(distance(0, 5, 0, 0)); // expected 5.0
+    System.out.print(quadratic(3, 4, 5));
   }
     // test your functions here to make sure they work
 
